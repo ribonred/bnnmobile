cba85b2ee4fd34a7c3a8574566b5daf4ea327a3b
agamw11 <agam.wardani@riseandshine-expo.com>
2020-06-13 12:15:40 +0800

    delete /

--- lib/widget/getlkn.dart
+++ lib/widget/getlkn.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'dart:async'; 
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import '../services//request.dart';
+import '../services/request.dart';
 
 class GetLkn extends StatefulWidget {
   @override
